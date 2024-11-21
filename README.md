5b3fd7bfa38c4db7506aba6db4ae44e53f5e7340
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-11-21 17:53:45 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,104 @@
+ # QUEEN-CHARITY
+   
+   <a>
+                                      <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Jersey+20+Charted&size=30&pause=1000&color=F71515&width=435&lines=BOT+100%25+LEGIT" alt="Typing SVG" /></a>   
+
+<p align="center"> 
+<up A simple WhatsApp User Bot Coded By CASEYRHODES 🌟</u>
+</p>
+<p align="center">
+<img src="https://files.catbox.moe/kwe3i5.jpg"/>       
+<p align="center">
+
+            
+<p align="center">
+<p align="center">
+  <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=EB+Garamond&weight=800&size=28&duration=4000&pause=1000&random=false&width=435&lines=+•★⃝ QUEEN-+CHARITY+★⃝•;MULTI-DEVICE+WHATSAPP+BOT;DEVELOPED+BY+CASEYWEB;RELEASED+DATE+05%2F11%2F2024" alt="Typing SVG" /></a>
+ 
+ ## Deployment Methods
+
+### 1. FORK THIS REPO
+
+<a href='https://github.com/caseyweb/QUEEN-CHARITY/fork' target="_blank"><img alt='Fork repo' src='https://img.shields.io/badge/Fork This Repo-black?style=for-the-badge&logo=git&logoColor=white'/></a>
+
+
+
+1.𝐆𝐄𝐓 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃 𝐅𝐑𝐎𝐌 𝐒𝐄𝐑𝐕𝐄𝐑
+
+
+  <a href="https://rhodespair-1303049c463e.herokuapp.com"><img title="GET-SESSION ID HERE" src="https://img.shields.io/badge/GET-SESSION ID HERE-h?color=green&style=for-the-badge&logo=nike" width="230" height="38.45"/></a></p>  
+  
+### DEPLOY ON RENDER
+
+1. If you don't have an account in RENDER, create one and deploy.
+    <br>
+    <a href='https://dashboard.render.com/select-repo?type=web' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/-DEPLOY-black?style=for-the-badge&logo=render&logoColor=white'/></a>
+
+
+
+ ### ✧✧ Follow this Tutorial for QUEEN_CHARITY free and safe Deployment
+
+  <a href="https://youtu.be/jD_8gFrCpm8?si=UuLSNlFONr1YUQRi"><img src="https://img.shields.io/badge/Tutorial-Video-ff0000?style=for-the-badge&logo=youtube&logoColor=ff000000&link=https://youtu.be/jD_8gFrCpm8?si=UuLSNlFONr1YUQRi" /><br>
+
+     
+### DEPLOY ON HEROKU
+
+1. If you don't have an account in Heroku, create one.
+    <br>
+    <a href='https://signup.heroku.com/' target="_blank"><img alt='Heroku' src='https://img.shields.io/badge/-Create-purple?style=for-the-badge&logo=heroku&logoColor=white'/></a>
+2. Now deploy.
+    <br>
+    <a href='https://dashboard.heroku.com/new?template=https://github.com/caseyweb/QUEEN-CHARITY' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/-DEPLOY-purple?style=for-the-badge&logo=heroku&logoColor=white'/></a>
+### DEPLOY ON REPLIT
+1. Deploy.
+    <br>
+    <a href='https://replit.com/github//caseyweb/QUEEN-CHARITY' target="_blank"><img alt='Replit' src='https://img.shields.io/badge/-Deploy-red?style=for-the-badge&logo=replit&logoColor=white'/></a>
+
+### `COMMANDS FOR TERMUX/UBUNTU`
+```bash
+apt update && apt upgrade -y
+pkg install proot-distro
+proot-distro install ubuntu
+proot-distro login ubuntu
+apt update && apt upgrade -y
+apt install -y webp git ffmpeg curl imagemagick
+apt -y remove nodejs
+curl -fsSl https://deb.nodesource.com/setup_lts.x | bash - && apt -y install nodejs
+git clone https://github.com/caseyweb/QUEEN-CHARITY
+cd QUEEN-CHARITY
+npm install
+npm start
+```
+
+--------
+
+
+
+
+## THIS SCRIPT IS NOT OPENLY ALLOWED TO USED IN ANY OF YOUR PROJECTS BE WARNED!!! 
+
+
+  
+  #### ```CASEYRHODES PROFILE VIEWS 🧚```
+![Visitor Count](https://profile-counter.glitch.me/caseyweb/count.svg)
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a><a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+
+
+## Support ME
+
+
+## ᴡʜᴀᴛsᴀᴘᴘ ᴄʜᴀɴɴᴇʟ
+<a href="https://whatsapp.com/channel/0029VakUEfb4o7qVdkwPk83E" target="_blank">
+    <img alt="whatsapp Group" src="https://img.shields.io/badge/ Whatsapp Support Channel -25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+  </a>
+</p>
+
+
+
+### Please Give One Star ✨ & [follow me notify my updates 💗](https://github.com/caseyweb)
+<b>Version -> 1.0.0</b>
+
+<a><img src='https://i.imgur.com/LyHic3i.gif'/></a><a><img src='https://i.imgur.com/LyHic3i.gif'/></a>
+  
+  ## ENJOY!!!;
---
+++ config.js
@@ -0,0 +1,77 @@
+//#ENJOY
+const fs = require("fs-extra");
+if (fs.existsSync(".env"))
+  require("dotenv").config({ path: __dirname + "/.env" });
+global.audio = "www.youtube.com";
+global.video = "www.youtube.com";
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "Verantechhub@gmail.com";
+global.location = "Ondo, Nigeria";
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://astrofx0011:astro@cluster0.lmwnxdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+global.allowJids = process.env.ALLOW_JID || "true";
+global.blockJids = process.env.BLOCK_JID || "null";
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
+global.github = process.env.GITHUB || "https://github.com/caseyweb/QUEEN-CHARITY";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VajIDgjDZ4LVnxfB1Z2s";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029VajIDgjDZ4LVnxfB1Z2s";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://files.catbox.moe/6xuv7u.jpg";
+global.devs = "2347082664317";
+global.sudo = process.env.SUDO || "2347082664317";
+global.owner = process.env.OWNER_NUMBER || "2347082664317";
+global.style = process.env.STYLE || "3";
+global.gdbye = process.env.GOODBYE || "true";
+global.wlcm = process.env.WELCOME || "true";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+global.disablegroup = process.env.DISABLE_GROUPS || "false",
+global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
+global.userImages = process.env.USER_IMAGES || "https://files.catbox.moe/kwe3i5.jpg";
+global.waPresence = process.env.WAPRESENCE || "available";
+global.readcmds = process.env.READ_COMMAND || "false";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "";
+global.read_status_from = process.env.READ_STATUS_FROM || "";
+
+global.api_smd = "https://api-smd-1.vercel.app";
+global.scan = "https://rhodespair-1303049c463e.herokuapp.com";
+
+global.SESSION_ID =
+  process.env.SESSION_ID ||
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiY090Y3YwNDhYWFovcWRxVzhFRjM5M2I0OXBBRDBnVUFkSHcxSGduRGkwZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK3I5QXR3alNyQThtdU13RWpwVkIwNGVORmYyR1FHajFuUlJEbmliWTBoYz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJtQ3RiN3czdVVzcGhnMU13Mno5S3ZnVTJoMThrYVNUZkQ5cXp1UHV3a0ZNPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJsUmx3a29RN1I0eWZwazlnRFdLNWpXZ1NiZGJBSTRacjZkUXhJWDRkK2pzPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik9HelBFUWt6LzVQUVRTOUlLRFVrbzgvbWtka0xoUllPL3dpYWpvWHN6VlE9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjVobngvMlRCK3hvbkg3cWNndk9tMHJ1Q0hkTVl4ZHduSjdWaDFDa1FJRzQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOEVJVXBzTEc0T1FKZ2VCVXpCSHRObzVLdVFmWWUzTmFncUVPY0hlQ3BIdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibDVHaXZWcW5pWGcyc3pKWHVDZ3BSSStMeWlxVUR3WW5mSnpGRUQvOWtEcz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkttZ0xtSHRBRkc4YUV5QzdYTnBhQXZlN1I2MUEvb3RpdktETzdqQWNvV055aEx3amVubXg2b25TSTgvTGdnOVdLUzJHYmpZS2xFL09xaXl3YXpaVUJBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTEsImFkdlNlY3JldEtleSI6ImFpd1kxYnpqWVdYQ3dUSmlRSHg5QXA2UHBBbTJoSHh5VDFreEpwU0wweGM9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjo2MSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjYxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IlRmTFpJVkYyVE5lQlFXaTBUUWJ0dkEiLCJwaG9uZUlkIjoiMDg2NTA4ZTItMmZmYi00YzExLTgxMmYtNGZjMzAxMmU3ZTgwIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJpc2tXUVNhZ2xrbXVyT3J5WHRDd0hGUTQ5VT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI1RWJMSEZzU0d6eVRFZW1ReXdheFpEa3lSTnc9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiWEFGRkE5UzYiLCJtZSI6eyJpZCI6IjI1NDExMjE5MjExOTo3NEBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDT2U4MW1ZUTZPL2h1UVlZQkNBQUtBQT0iLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoibDk0V1U3S1R4M2hzQVZVMjlMdGlLdVJhbGRObFVEMzJGa0hRU3VOcVMxcz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiREt1QUFCY1lrK1NhVEI5cUtwZ2hQVDRSblE5dlZNRHk3RWYvRDNNWW5WUkZHVGZaRmdoa3dlYVV0NWRQWjB1aTNJa3V6S3FDYWNENEFGZndEZkJ2Q0E9PSIsImRldmljZVNpZ25hdHVyZSI6Imc4M3N3S3V3NUt4Nmlpd05vdkwraHpCMmNqNnZxVVppVTdRN2ZRS1ZZdzdsNjc3ODV2RjlTQ1ZYTEd2L1BtM0dWRjFibkdPR0p5THI4b0VqQVh5RkFBPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjU0MTEyMTkyMTE5Ojc0QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlpmZUZsT3lrOGQ0YkFGVk52UzdZaXJrV3BYVFpWQTk5aFpCMEVyamFrdGIifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MzE3NTM5NzQsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBT29hIn0=" // session id here
+module.exports = {
+  menu: process.env.MENU || "2",
+  HANDLERS: process.env.PREFIX || ".",
+  BRANCH: process.env.BRANCH || "main",
+  VERSION: process.env.VERSION || "1.0.0",
+  caption: process.env.CAPTION || "`QUEEN_CHARITY™`",
+  author: process.env.PACK_AUTHER || "𒀱ꪳ𓊈𒆜𓂀ᴸᴼᴿᴰℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊𓂀𒆜𓊉 𒀱",
+  packname: process.env.PACK_NAME || "𝐂𝐀𝐒𝐄𝐘𝐑𝐇𝐎𝐃𝐄𝐒 𝐓𝐄𝐂𝐇",
+  botname: process.env.BOT_NAME || "`𝗤𝗨𝗘𝗘𝗡_𝗖𝗛𝗔𝗥𝗜𝗧𝗬`",
+  ownername: process.env.OWNER_NAME || "𒀱ꪳ𓊈𒆜𓂀ᴸᴼᴿᴰℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊𓂀𒆜𓊉 𒀱",
+  errorChat: process.env.ERROR_CHAT || "",
+  KOYEB_API: process.env.KOYEB_API || "false",
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  antilink_values: process.env.ANTILINK_VALUES || "all",
+  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+  aitts_Voice_Id: process.env.AITTS_ID || "37",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
+  LANG: (process.env.THEME || "N I K K A").toUpperCase(),
+};
+global.rank = "updated";
+global.isMongodb = true;
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(`Update'${__filename}'`);
+  delete require.cache[file];
+  require(file);
+});
