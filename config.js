991fc8d1041a6901a0f4194e08837a6af4f0000c
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-11-24 08:33:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
 
 global.api_smd = "https://api-smd-1.vercel.app";
-global.scan = "https://rhodespair-1303049c463e.herokuapp.com";
+global.scan = "https://veran-session-id.onrender.com";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
